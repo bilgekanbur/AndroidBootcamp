f2130c0e6f88b221a0ef78fa023f7470718e5af5
bilgekanbur <bilgesenakanbur@gmail.com>
2025-04-26 18:27:13 +0300

    bootcamp ilk ödev dosyası yüklendi.

---
+++ app/src/main/java/com/example/kotlin_bootcamp_odev/IlkOdev/DegiskenTanimlama.kt
@@ -0,0 +1,110 @@
+package com.example.kotlin_bootcamp_odev.IlkOdev
+
+
+
+fun main() {
+    val sehir = "Ankara"
+    val ulke = "Turkiye"
+    val telefon = 123456789
+    val postaKodu = 66520
+    val email = "bilgesenakanbur@gmail.com"
+    val meslek = "Bilgisayar Mühendisi"
+    val stokMiktari = 150000
+    val musteriAdi = "Bilge kanbur"
+    val bakiye = 2000
+    val dogumGunu = "07.10.2002"
+    val maas = 3000
+    val medeniDurum = "Bekar"
+    val urunYorum = "Lütfen almayın çok kötü!"
+
+    val odemeTarihi = "25.04.2025"
+    val odeme = "Kredi Karti"
+    val siparisAdeti = 3
+    val arabaModeli = "Volvo S60"
+    val kitapAdi = "Usta ve Margarita"
+    val yayinlamaTarihi = "2020"
+    val indirimMiktari = 15.5
+    val odaSayisi = 2
+    val enlem = 41.0082
+    val boylam = 28.9784
+    val urunAdi = "Laptop"
+    val yemekFiyati = 150.0
+
+    val marka = "Apple"
+    val muzikAdi = "Düldül"
+    val videoSuresi = "2 saat"
+    val urunPuani = 4.8
+    val resimAdi = "manzara.jpg"
+    val dosyaFormati = "JPEG"
+    val renk = "Beyaz"
+    val renkKodu = "#0000FF"
+    val telefonModeli = "iPhone 11"
+    val ekranBoyutu = 6.1
+    val agirlik = 174
+
+    val ulusalGun = "Cumhuriyet Bayrami"
+    val tatilGunu = "23 Nisan"
+    val rezervasyonTarihi = "25.05.2025"
+    val sokakAdi = "İstasyon Caddesi"
+    val otobusHatti = "34AS"
+    val kalanDakika = 5
+    val takipKodu = "ABC123456789"
+    val kuponSuresi = "30 gun"
+    val kuponKodu = "KUPON30"
+    val faturaAdresi = "Yakutiye, Erzurum"
+
+    println("Degiskenler basariyla olusturuldu ve deger atamasi yapildi.")
+
+    println("Sehir: $sehir")
+    println("Ulke: $ulke")
+    println("Telefon: $telefon")
+    println("Posta Kodu: $postaKodu")
+    println("Email: $email")
+    println("Meslek: $meslek")
+    println("Stok Miktari: $stokMiktari")
+    println("Musteri Adi: $musteriAdi")
+    println("Bakiye: $bakiye")
+    println("Dogum Gunu: $dogumGunu")
+    println("Maas: $maas")
+    println("Medeni Durum: $medeniDurum")
+    println("Urun Yorum: $urunYorum")
+
+    println("Odeme Tarihi: $odemeTarihi")
+    println("Odeme: $odeme")
+    println("Siparis Adeti: $siparisAdeti")
+    println("Araba Modeli: $arabaModeli")
+    println("Kitap Adi: $kitapAdi")
+    println("Yayinlama Tarihi: $yayinlamaTarihi")
+    println("Indirim Miktari: $indirimMiktari")
+    println("Oda Sayisi: $odaSayisi")
+    println("Enlem: $enlem")
+    println("Boylam: $boylam")
+    println("Urun Adi: $urunAdi")
+    println("Yemek Fiyati: $yemekFiyati")
+
+    println("Marka: $marka")
+    println("Muzik Adi: $muzikAdi")
+    println("Video Suresi: $videoSuresi")
+    println("Urun Puani: $urunPuani")
+    println("Resim Adi: $resimAdi")
+    println("Dosya Formati: $dosyaFormati")
+    println("Renk: $renk")
+    println("Renk Kodu: $renkKodu")
+    println("Telefon Modeli: $telefonModeli")
+    println("Ekran Boyutu: $ekranBoyutu")
+    println("Agirlik: $agirlik")
+
+    println("Ulusal Gun: $ulusalGun")
+    println("Tatil Gunu: $tatilGunu")
+    println("Rezervasyon Tarihi: $rezervasyonTarihi")
+    println("Sokak Adi: $sokakAdi")
+    println("Otobus Hatti: $otobusHatti")
+    println("Kalan Dakika: $kalanDakika")
+    println("Takip Kodu: $takipKodu")
+    println("Kupon Suresi: $kuponSuresi")
+    println("Kupon Kodu: $kuponKodu")
+    println("Fatura Adresi: $faturaAdresi")
+}
+
+
+
